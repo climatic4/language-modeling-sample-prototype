f3639c87f4fe2156a01066ee2ba056435d3189d6
Thierry Bélair <climatic4@gmail.com>
2018-11-17 13:23:58 -0500

    Florie est la meilleure

---
+++ main.py
@@ -0,0 +1,5 @@
+if __name__ == "__main__":
+    with open("/home/t/develop/language_modelling/ress/test.txt", "r") as mui_mui :
+        phrases = mui_mui.readlines()
+    for p in phrases :
+        print(p)
